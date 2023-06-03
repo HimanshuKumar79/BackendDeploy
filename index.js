44feca65cd3dd2ea451ed92a7d9803bc224e8d14
Himanshu Kumar <64852520+HimanshuKumar79@users.noreply.github.com>
2023-06-03 20:21:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const route = require("./routes/route");
 const app = express();
-
+const cors=require("cors")
+mongoose.set("striceQurey",true)
 app.use("/api/v1", route);
-
+app.use(cors())
 app.listen(8000, () => {
   console.log("Sercerts listening on 8000");
 });
