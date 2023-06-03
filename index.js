bbc701ad8f9aad4808f12d57b58f2293814dea90
Himanshu Kumar <64852520+HimanshuKumar79@users.noreply.github.com>
2023-06-03 20:30:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const route = require("./routes/route");
 const app = express();
-const cors=require("cors")
 app.use("/api/v1", route);
-app.use(cors())
 app.listen(8000, () => {
   console.log("Sercerts listening on 8000");
 });
