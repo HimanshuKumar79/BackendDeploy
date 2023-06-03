c8df2915ee51508f8fc0fe702d30707cafd46efe
Himanshu Kumar <64852520+HimanshuKumar79@users.noreply.github.com>
2023-06-03 20:41:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const route = require("./routes/route");
 const app = express();
-app.use("/api/v1", route);
+app.use("", route);
 app.listen(8000, () => {
   console.log("Sercerts listening on 8000");
 });
